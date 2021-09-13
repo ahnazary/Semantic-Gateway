53d66de907640e402bb173a0b5b6d284a486db05
ahnazary <49793253+ahnazary@users.noreply.github.com>
2021-09-13 21:27:55 +0200

    SVM added - some bugs exist

--- Broker/src/Main.java
+++ Broker/src/Main.java
@@ -1,30 +1,40 @@
 import java.io.IOException;
 import java.text.ParseException;
 
-import org.apache.spark.sql.SparkSession;
+//import java.util.Arrays;
+//import java.util.List;
+//import org.apache.spark.ml.feature.Word2Vec;
+//import org.apache.spark.ml.feature.Word2VecModel;
+//import org.apache.spark.ml.linalg.Vector;
+//import org.apache.spark.sql.Dataset;
+//import org.apache.spark.sql.Row;
+//import org.apache.spark.sql.RowFactory;
+//import org.apache.spark.sql.SparkSession;
+//import org.apache.spark.sql.types.*;
+
 
 public class Main {
 
 	public static void main(String[] args) throws IOException, ParseException {
 	
-//		SurfaceForm Test = new SurfaceForm("input","saref.ttl");
-//		Test.exactQuery();
-//		Test.morphemesQuery();
-//		
-//		
-//		WriteJSON Sensors = new WriteJSON();
-//		Sensors.writeJSONFile();
-//		
-//		ReadJSON rs = new ReadJSON("/home/amirhossein/Documents/GitHub/semantic-broker/Broker/input_3");
-//		rs.returnKeys();
+		SurfaceForm Test = new SurfaceForm("input","saref.ttl");
+		Test.exactQuery();
+		Test.morphemesQuery();
 		
 		
-		SparkSession spark = SparkSession
-			      .builder()
-			      .appName("JavaWord2VecExample")
-			      .getOrCreate();
-			    // $example on$
-			    // Input data: Each row is a bag of words from a sentence or document.
+		WriteJSON Sensors = new WriteJSON();
+		Sensors.writeJSONFile();
+		
+		ReadJSON rs = new ReadJSON("/home/amirhossein/Documents/GitHub/semantic-broker/Broker/input_3");
+		rs.returnKeys();
+		
+//		
+//		SparkSession spark = SparkSession
+//			      .builder()
+//			      .appName("JavaWord2VecExample")
+//			      .getOrCreate();
+//
+//			    // Input data: Each row is a bag of words from a sentence or document.
 //			    List<Row> data = Arrays.asList(
 //			      RowFactory.create(Arrays.asList("Hi I heard about Spark".split(" "))),
 //			      RowFactory.create(Arrays.asList("I wish Java could use case classes".split(" "))),
@@ -49,11 +59,8 @@ public class Main {
 //			      Vector vector = (Vector) row.get(1);
 //			      System.out.println("Text: " + text + " => \nVector: " + vector + "\n");
 //			    }
-//			    // $example off$
+//
 //			    spark.stop();
-//		
-	}	
-	
-	
-	
+		
+	}		
 }
